51f37721236635e70cf22eb253beb9e29f7e2561
Tomas0998 <52677969+Tomas0998@users.noreply.github.com>
2019-07-12 12:03:51 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,17 @@ function mostrar()
 {
 //tomo la edad  
 
+var adolescente;
+
+adolescente = document.getElementById('edad').value;
+
+if(adolescente >=13)
+
+
+
+if(adolescente<=17)
+	{ alert("adolescente"); }
+
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+
+edad=document.getElementById('edad').value;
+
+if (edad>=18) {alert("mayor de edad");}
+
+if (edad<13) {alert("menor de edad");}
+
+if (edad>=13)
+if (edad<=17) {alert("adolescente");}
 
 
 
